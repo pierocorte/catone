b17d521ed00f5adde1d225f6c1e65664ec757ac0
Piero Corte <p.corte@farm4trade.com>
2026-03-10 12:09:09 +0100

    fixex start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 cd ./agw3000 && pnpm start &
 cd ./reg_csv_passive && pnpm start &
 cd ./libs && pnpm start &
-cd ./FakeProcessPassiveRegistryRestReplay && pnpm start &
+cd ./FakeProcessPassiveRegistryRestReply && pnpm start &
 cd ./fe3020_registry && pnpm start &
 wait
