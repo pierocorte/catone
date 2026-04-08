01e246a78c70288d94daffd064945a8e77dfd642
Marco <marco.pratesi@hotmail.it>
2026-04-08 11:45:04 +0200

    aggiunto frontend per registry fe3020

--- trailSv/index.js
+++ trailSv/index.js
@@ -2,18 +2,17 @@ import TRAILSV from "./TrailSv.js";
 
 async function main() {
   try {
-    await TRAILSV.register();
-    await TRAILSV.setOn();
+    await TRAILSV.on();
 
     console.log("waiting...");
 
-    // setTimeout(async () => {
-    //   await TRAILSV.setOff();
-    // }, 5000);
+    setTimeout(async () => {
+      await TRAILSV.off();
+    }, 5000);
 
-    // setTimeout(async () => {
-    //   await TRAILSV.shutdown();
-    // }, 10000);
+    setTimeout(async () => {
+      await TRAILSV.shutdown();
+    }, 10000);
   } catch (err) {
     console.log(err);
   }
